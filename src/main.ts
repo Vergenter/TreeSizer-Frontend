c253c2565a7da912d9e8f13ca21db90dcd349671
Vergenter <okolok1998@gmail.com>
2020-12-17 02:39:21 +0100

    added support for vue-panzoom

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// import vue-panzoom
+import panZoom from "vue-panzoom";
 
+Vue.use(panZoom);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Design.vue
+++ src/views/Design.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="design">
-    <h1>This is an design page</h1>
-    <div>a{{ $route.params.tree}}a</div>
-  </div>
+  <!-- <div class="black"></div> -->
+  <panZoom
+    :options="{ minZoom: 0.5, maxZoom: 5, initialZoom: 1 }"
+    class="panzoom"
+  >
+    <div class="black"></div>
+  </panZoom>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-})
+@Component({})
 export default class Design extends Vue {}
 </script>
+
+<style scoped>
+.panzoom {
+  height: 100%;
+  width: 100%;
+}
+.panzoom >>> .vue-pan-zoom-scene {
+  height: 100%;
+  width: 100%;
+}
+.black {
+  height: 100px;
+  width: 100px;
+  background-color: black;
+}
+</style>
