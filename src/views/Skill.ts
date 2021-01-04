eee18aef35bb1a735e38402526294b662560a145
Vergenter <okolok1998@gmail.com>
2021-01-04 14:35:20 +0100

    fix error for ci cd

--- src/views/Skill.ts
+++ src/views/Skill.ts
@@ -1,5 +1,6 @@
 import { Eq } from "fp-ts/lib/Eq";
 import { Ord } from "fp-ts/lib/Ord";
+import { Option } from "fp-ts/lib/Option";
 export enum SkillType {
   normal,
   ghost,
@@ -16,7 +17,7 @@ export interface Skill {
   name: string;
   description: string;
   tier: number;
-  colorGroup: number;
+  colorGroup: Option<number>;
 }
 export const skillEq: Eq<Skill> = {
   equals: (s1: Skill, s2: Skill) => s1.id === s2.id
