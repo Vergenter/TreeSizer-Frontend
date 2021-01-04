8aa902aa2e9cb08580b7a09f0e242c452e2a9c51
Vergenter <okolok1998@gmail.com>
2021-01-04 14:30:01 +0100

    fix unlimited template tier

--- src/views/skillTemplate.ts
+++ src/views/skillTemplate.ts
@@ -2,15 +2,16 @@ const SkillPerTier = 8;
 import { range, difference, head } from "fp-ts/lib/Array";
 import { eqNumber } from "fp-ts/lib/Eq";
 import { pipe } from "fp-ts/lib/function";
-import { map } from "fp-ts/lib/Option";
+import { map, none, some, chain } from "fp-ts/lib/Option";
 import { SkillType } from "./Skill";
 export function getTemplate(idsTaken: number[]) {
   return (tier: number) =>
     pipe(
       tier,
-      val => range((val - 1) * SkillPerTier, val * SkillPerTier - 1),
-      difference(eqNumber)(idsTaken),
-      head,
+      tier => (tier >= 0 && tier < 64 ? some(tier) : none),
+      map(val => range((val - 1) * SkillPerTier, val * SkillPerTier - 1)),
+      map(difference(eqNumber)(idsTaken)),
+      chain(head),
       map(id => ({
         type: SkillType.template,
         selected: false,
@@ -18,7 +19,7 @@ export function getTemplate(idsTaken: number[]) {
         name: `template ${id}`,
         description: "generic template skill",
         tier,
-        colorGroup: 0
+        colorGroup: none
       }))
     );
 }
